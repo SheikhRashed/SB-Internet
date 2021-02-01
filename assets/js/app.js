988af28443a09aecc2fe696bc5b53b6d30454509
Sheikh Rashed <mdrashed1310@gmail.com>
2021-02-01 14:22:11 +0600

    Feed section completed

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$('.feed__slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    dots: false,
+    prevArrow:
+        '<button class="prevArrow"> <i class="fas fa-chevron-left"></i></button>',
+    nextArrow:
+        '<button class="nextArrow"> <i class="fas fa-chevron-right"></i></button>',
+});
